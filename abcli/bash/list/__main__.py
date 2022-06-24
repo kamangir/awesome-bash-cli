8327aa94089ba6d0b50ea8ee46ef4cb0320a9252
kamangir <arash@kamangir.net>
2022-06-24 17:08:14 -0400

    validating abcli init - kamangir/bolt#685

--- abcli/bash/list/__main__.py
+++ abcli/bash/list/__main__.py
@@ -4,9 +4,10 @@ from ... import *
 
 name = f"{shortname}.bash.list"
 
+list_of_tasks = "in,len,nonempty,resize,sort"
 
 parser = argparse.ArgumentParser(name, description=f"{name}-{version}")
-parser.add_argument("task", type=str, help="in,len,nonempty,resize,sort")
+parser.add_argument("task", type=str, help=list_of_tasks)
 parser.add_argument("--count", type=int)
 parser.add_argument("--delim", type=str, default=",")
 parser.add_argument("--item", type=str)
@@ -17,7 +18,7 @@ delim = " " if args.delim == "space" else "," if not args.delim else args.delim
 
 list_of_items = [thing for thing in args.items.split(delim) if thing]
 
-success = False
+success = args.task in list_of_tasks.split(",")
 if args.task == "in":
     print("True" if args.item in list_of_items else "False")
 elif args.task == "len":
