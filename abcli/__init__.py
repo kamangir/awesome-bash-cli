7c2c336dece89320274868449f2b40d5e0e2cbaf
kamangir <arash@kamangir.net>
2022-06-27 14:42:14 -0400

    validating abcli init - kamangir/bolt#685

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -2,6 +2,6 @@ name = "awesome_bash_cli"
 
 shortname = "abcli"
 
-version = "1.1.94"
+version = "1.1.95"
 
 description = "a framework for quickly building awesome bash cli's."
--- bash/install/papertrail.sh
+++ bash/install/papertrail.sh
@@ -3,9 +3,9 @@
 abcli_papertrail_revision="104"
 
 if [ -f "$abcli_path_git/abcli_install_papertrail_${abcli_papertrail_revision}_complete" ] ; then
-    abcli_log "verified papertrail $abcli_papertrail_revision."
+    abcli_log "verified papertrail-$abcli_papertrail_revision."
 else
-    abcli_log "installing papertrail $abcli_papertrail_revision..."
+    abcli_log "installing papertrail-$abcli_papertrail_revision..."
 
     # https://github.com/papertrail/remote_syslog2/releases/tag/v0.20
     if [[ "$abcli_is_mac" == true ]] ; then
