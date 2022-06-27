abac97c1c1076872e5d0f82449be2a2101134999
kamangir <arash@kamangir.net>
2022-06-27 14:02:29 -0400

    env - kamangir/bolt#685

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -2,6 +2,6 @@ name = "awesome_bash_cli"
 
 shortname = "abcli"
 
-version = "1.1.75"
+version = "1.1.77"
 
 description = "a framework for quickly building awesome bash cli's."
---
+++ bash/tasks/env.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+function abcli_env() {
+    local task=$(abcli_unpack_keyword $1)
+
+    if [ "$task" == "help" ] ; then
+        abcli_help_line "abcli env [keyword_1]" \
+            "show environment variables [relevant to keyword_1]."
+        return
+    fi
+
+    env | grep abcli_ | grep "$1" | sort
+}
