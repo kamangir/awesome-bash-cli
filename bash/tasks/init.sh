2b51ddbe231427f1615fef6ad8a2409263993af0
kamangir <arash@kamangir.net>
2022-06-27 14:36:08 -0400

    validating abcli init - kamangir/bolt#685

---
+++ bash/tasks/init.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+
+function abcli_init() {
+    local task=$(abcli_unpack_keyword $1)
+
+    if [ "$task" == "help" ] ; then
+        abcli_help_line "init [plugin_1]" \
+            "init [plugin_1]."
+        return
+    fi
+
+    local plugin_name=$task
+
+    local current_path=$(pwd)
+
+    if [ -z "$plugin_name" ] ; then
+        source $abcli_path_abcli/bash/main.sh
+    else
+        local repo_name=$(echo "$plugin_name" | tr _ -)
+
+        for filename in $abcli_path_git/$repo_name/abcli/*.sh ; do
+            source $filename
+        done
+    fi
+
+    if [[ "$current_path" == "$abcli_path_git"* ]] ; then
+        cd $current_path
+    fi
+}
