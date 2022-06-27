d54a1fd4babcf55fc0b68f1cf3d0bc30a200a226
kamangir <arash@kamangir.net>
2022-06-27 13:00:50 -0400

    abcli terraform - kamangir/bolt#685

---
+++ bash/tasks/terraform.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+
+function abcli_ec2_terraform_get() {
+    echo $(python3 -c "import abcli.file as file; print(file.load_json('$abcli_path_abcli/assets/aws/info.json')[1]['ec2']$1)")
+}
+
+function abcli_terraform() {
+    local task=$(abcli_unpack_keyword $1)
+
+    if [ "$task" == "help" ] ; then
+        abcli_help_line "terraform disable" \
+            "disable terraform."
+        abcli_help_line "terraform enable" \
+            "enable terraform."
+
+        if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
+            python3 -m abcli.terraform --help
+        fi
+        return
+    fi
+
+    if [ "$task" == "disable" ] ; then
+        touch $abcli_path_bash/abcli_disabled
+        return
+    fi
+
+    if [ "$task" == "enable" ] ; then
+        rm $abcli_path_bash/abcli_disabled
+        return
+    fi
+
+    # wip
+}
