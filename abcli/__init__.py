af8322e290aa0bf7fb442b5df47fd1d629b338e6
kamangir <arash@kamangir.net>
2022-06-28 12:03:48 -0400

    validating 'bracket publish' - kamangir/bolt#685

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -1,7 +1,11 @@
+import os
+
 name = "awesome_bash_cli"
 
 shortname = "abcli"
 
-version = "1.1.133"
+version = "1.1.134"
+
+fullname = f"{shortname}-{version}-{os.getenv('bolt_git_branch')}"
 
 description = "a framework for quickly building awesome bash cli's."
