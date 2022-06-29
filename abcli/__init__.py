9ed204f9ffecfaac481ce3ac3c1b375bc4ead475
kamangir <arash@kamangir.net>
2022-06-29 12:07:59 -0400

    speed up - kamangir/bolt#685

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -1,11 +1,10 @@
 import os
 
 name = "awesome_bash_cli"
-
 shortname = "abcli"
-
-version = "1.1.148"
-
-fullname = f"{shortname}-{version}-{os.getenv('abcli_git_branch')}"
-
+version = "1.1.149"
 description = "a framework for quickly building awesome bash cli's for machine vision/deep learning."
+
+
+def fullname():
+    return f"{shortname}-{version}-{os.getenv('abcli_git_branch')}"
