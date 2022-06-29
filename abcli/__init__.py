b2f448520ec57574bc0bc3804537a181e7437fbf
kamangir <arash@kamangir.net>
2022-06-29 10:34:45 -0400

    += table - kamangir/bolt#685

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -4,7 +4,7 @@ name = "awesome_bash_cli"
 
 shortname = "abcli"
 
-version = "1.1.146"
+version = "1.1.147"
 
 fullname = f"{shortname}-{version}-{os.getenv('abcli_git_branch')}"
 
---
+++ abcli/table.py
@@ -0,0 +1,176 @@
+import os
+import os.path
+import pymysql
+from . import file
+from . import logging
+from .logging import crash_report
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Table(object):
+    def __init__(self, name):
+        self.name = name
+
+        _, info = file.load_json(
+            os.path.join(os.getenv("abcli_path_abcli"), "assets/aws/info.json")
+        )
+        self.db = info["rds"]["db"]
+        self.host = info["rds"]["host"]
+        self.password = info["rds"]["password"]
+        self.port = info["rds"]["port"]
+        self.user = info["rds"]["user"]
+
+        self.connection = None
+
+    def connect(self, create_command=""):
+        """connect to self.
+
+        Args:
+            create_command (str, optional): create command. Defaults to "".
+
+        Returns:
+            bool: success.
+        """
+        if self.connection is not None:
+            self.disconnect()
+
+        try:
+            self.connection = pymysql.connect(
+                self.host,
+                user=self.user,
+                port=self.port,
+                passwd=self.password,
+                db=self.db,
+            )
+        except:
+            crash_report("-abcli.table: connect: failed.")
+            return False
+
+        return True if not create_command else self.create(create_command)
+
+    def create(self, create_command):
+        """create self.
+
+        Args:
+            create_command (str): create command.
+
+        Returns:
+            bool: success.
+        """
+        return self.execute(
+            "CREATE TABLE IF NOT EXISTS {} ({})".format(
+                self.name,
+                ",".join(
+                    [
+                        "id INT(24) NOT NULL AUTO_INCREMENT",
+                        "timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP",
+                    ]
+                    + create_command
+                    + ["PRIMARY KEY (`id`)", "INDEX `index_timestamp` (`timestamp`)"]
+                ),
+            ),
+            commit=True,
+        )
+
+    def disconnect(self):
+        """disconnect.
+
+        Returns:
+            bool: success.
+        """
+        if self.connection is None:
+            return True
+
+        success = True
+        try:
+            self.connection.close()
+        except:
+            crash_report("-abcli.table: disconnect: failed.")
+            success = False
+
+        self.connection = None
+        return success
+
+    def drop(self):
+        """drop self.
+
+        Returns:
+            bool: success.
+        """
+        return self.execute(f"DROP table {self.name};")
+
+    def execute(self, sql, commit=True, returns_output=False):
+        """execute sql command.
+
+        Args:
+            sql (str): sql command.
+            commit (bool, True): this command causes a change, commit. Defaults to True.
+            returns_output (bool, optional): this command returns some output. Defaults to False.
+
+        Returns:
+            bool: success.
+            Any: output, optional.
+        """
+        output = []
+        success = False
+        try:
+            with self.connection.cursor() as cursor:
+                if isinstance(sql, tuple):
+                    cursor.execute(sql[0], sql[1])
+                else:
+                    cursor.execute(sql)
+
+                if returns_output:
+                    output = cursor.fetchall()
+
+                if commit:
+                    # connection is not autocommit by default. So you must commit to save
+                    # your changes.
+                    self.connection.commit()
+
+            success = True
+        except:
+            crash_report(f"-abcli.table: execute({sql}): failed.")
+
+        return (success, output) if returns_output else success
+
+    def insert(self, columns, values):
+        """insert data in self.
+
+        Args:
+            columns (List[str]): list of columns to insert.
+            values (List[Any]): list of values to insert.
+
+        Returns:
+            bool: success
+        """
+        return self.execute(
+            (
+                f"INSERT INTO {self.name}"
+                + " ("
+                + ", ".join(columns)
+                + ") VALUES ("
+                + ", ".join(len(columns) * ["%s"])
+                + ")",
+                values,
+            )
+        )
+
+    def read(self, columns, condition):
+        """read data from self.
+
+        Args:
+            columns (List[str]): list of column to read.
+            condition (str): condition.
+
+        Returns:
+            bool: success
+            List[Any]: content that is read from self.
+        """
+        return self.execute(
+            f"SELECT {', '.join(columns)} FROM {self.name} WHERE {condition}",
+            commit=False,
+            returns_output=True,
+        )
