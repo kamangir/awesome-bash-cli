9117ccd193dfd5215481fe792b0914c87a939deb
kamangir <arash@kamangir.net>
2022-07-03 13:52:02 -0400

    validating relations - kamangir/bolt#685

--- bash/plugins/relations.sh
+++ bash/plugins/relations.sh
@@ -13,10 +13,10 @@ function abcli_relation() {
             "get relation between object_1 and object_2."
         abcli_help_line "relation list" \
             "list possible relations."
-        abcli_help_line "relation search object_1" \
-            "search for all relations of object_1."
+        abcli_help_line "relation search object_1 [--relation relation_1]" \
+            "search for all relations of/relation_1 to object_1."
         abcli_help_line "relation set object_1 object_2 relation_1 [validate]" \
-            "set object_1 -relation_1-> object_2 [and validate]."
+            "set object_1 =relation_1=> object_2 [and validate]."
 
         if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
             python3 -m abcli.plugins.relations --help
