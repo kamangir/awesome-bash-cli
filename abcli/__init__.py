202273dceae1a0d9311d0410fff760427188c779
kamangir <arash@kamangir.net>
2022-07-04 15:48:11 -0400

    clean-up - kamangir/bolt#685

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -4,7 +4,7 @@ name = "awesome_bash_cli"
 
 shortname = "abcli"
 
-version = "1.1.347"
+version = "1.1.348"
 
 description = "a framework for quickly building awesome bash cli's for machine vision/deep learning."
 
