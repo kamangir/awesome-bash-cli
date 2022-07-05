f6d4ac00dd5b28bc57f1365cadc8d320d63765cd
kamangir <arash@kamangir.net>
2022-07-05 10:59:18 -0400

    += image_classifier - kamangir/bolt#681

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -4,7 +4,7 @@ name = "awesome_bash_cli"
 
 shortname = os.getenv("abcli_name")
 
-version = "1.1.448"
+version = "1.1.449"
 
 description = "a framework for quickly building awesome bash cli's for machine vision/deep learning."
 
