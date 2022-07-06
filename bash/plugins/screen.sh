07b46fa393013af81b8ef30b754295efd17120d0
kamangir <arash@kamangir.net>
2022-07-06 11:51:56 -0400

    += screen - kamangir/todo#133

---
+++ bash/plugins/screen.sh
@@ -0,0 +1,40 @@
+#! /usr/bin/env bash
+
+function abcli_screen() {
+    local task=$(abcli_unpack_keyword $1)
+
+    if [ "$task" == "help" ] ; then
+        abcli_help_line "screen [name_1]" \
+            "start a screen [and call it name_1].".
+        abcli_help_line "screen kill" \
+            "kill all screens."
+        abcli_help_line "screen list" \
+            "list screens."
+        abcli_help_line "screen resume [name_1]" \
+            "resume screen [named name_1]."
+        return
+    fi
+
+    if [ "$task" == "kill" ] ; then
+        # https://unix.stackexchange.com/a/94528
+        killall screen
+        return
+    fi
+
+    if [ "$task" == "list" ] ; then
+        screen -ls
+        return
+    fi
+
+    if [ "$task" == "resume" ] ; then
+        screen -r ${@:2}
+        return
+    fi
+
+    local screen_name="$1"
+    if [ -z "$screen_name" ]; then
+        local screen_name="abcli-$abcli_object_name"
+    fi
+
+    screen -q -S $screen_name -t $screen_name
+}
