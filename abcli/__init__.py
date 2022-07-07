ca29d79a9403a08b8cb265280888f24a299b8273
kamangir <arash@kamangir.net>
2022-07-06 20:55:34 -0400

    += log_level, plot_level - kamangir/bolt#681

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -4,10 +4,19 @@ name = "awesome_bash_cli"
 
 shortname = os.getenv("abcli_name")
 
-version = "1.1.476"
+version = "1.1.477"
 
 description = "a framework for quickly building awesome bash cli's for machine vision/deep learning."
 
 
 def fullname():
     return f"{shortname}-{version}-{os.getenv('abcli_git_branch')}"
+
+
+LOG_ON = 1
+LOG_VERBOSE = 2
+log_level = LOG_ON
+
+PLOT_ON = 1
+PLOT_ALL = 2
+plot_level = PLOT_ON
