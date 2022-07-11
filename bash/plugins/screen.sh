6a00ea64b58313aa1f22e4c6f50584e276992f23
kamangir <arash@kamangir.net>
2022-07-11 17:11:03 -0400

    clean-up - kamangir/bolt#685

--- bash/plugins/screen.sh
+++ bash/plugins/screen.sh
@@ -4,13 +4,13 @@ function abcli_screen() {
     local task=$(abcli_unpack_keyword $1)
 
     if [ "$task" == "help" ] ; then
-        abcli_help_line "screen [name_1]" \
+        abcli_help_line "$abcli_cli_name screen [name_1]" \
             "start a screen [and call it name_1].".
-        abcli_help_line "screen kill" \
+        abcli_help_line "$abcli_cli_name screen kill" \
             "kill all screens."
-        abcli_help_line "screen list" \
+        abcli_help_line "$abcli_cli_name screen list" \
             "list screens."
-        abcli_help_line "screen resume [name_1]" \
+        abcli_help_line "$abcli_cli_name screen resume [name_1]" \
             "resume screen [named name_1]."
         return
     fi
