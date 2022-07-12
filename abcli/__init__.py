5f6419302bcada8ff55f66c0c305850909120c63
kamangir <arash@kamangir.net>
2022-07-12 10:34:02 -0400

    += browser - kamangir/bolt#687

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -4,7 +4,7 @@ name = "awesome_bash_cli"
 
 shortname = os.getenv("abcli_name")
 
-version = "1.1.534"
+version = "1.1.535"
 
 description = "a framework for quickly building awesome bash cli's for machine vision/deep learning."
 
