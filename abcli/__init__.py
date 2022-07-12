688c7dad7d026b79b950a0c24a8a5f70735bbfd7
kamangir <arash@kamangir.net>
2022-07-12 14:38:31 -0400

    validating mayo-clinic-strip-ai - kamangir/bolt#689

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -4,7 +4,7 @@ name = "awesome_bash_cli"
 
 shortname = os.getenv("abcli_name")
 
-version = "1.1.547"
+version = "1.1.548"
 
 description = "a framework for quickly building awesome bash cli's for machine vision/deep learning."
 
@@ -14,7 +14,7 @@ def fullname():
 
 
 LOG_ON = 1
-LOG_VERBOSE = 2
+LOG_ALL = 2
 log_level = LOG_ON
 
 PLOT_ON = 1
