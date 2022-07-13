aeeb15aa666f7fe15ff16e11f115d5f54444aed5
kamangir <arash@kamangir.net>
2022-07-13 13:08:57 -0400

    validating fashion_mnist train - kamangir/bolt#689

--- abcli/plugins/graphics/distribution.py
+++ abcli/plugins/graphics/distribution.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from .signature import add_signature
+from ... import file
 from ... import string
 from ... import logging
 import logging
@@ -5,34 +9,10 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def render_distribution(distribution, class_names, filename, options=""):
-    """
-    render distribution
-    :param distribution: distribution
-    :param class_names: class_names
-    :param filename: filename
-    :param options:
-        . footer : footer.
-                   default: []
-        . header : header.
-                   default: []
-        . size   : size.
-                   default: 8
-        . title : title.
-                   default: ""
-        . file.save_image()
-    :return: success
-    """
-
-    options = (
-        Options(options)
-        .default("header", [])
-        .default("footer", [])
-        .default("size", 8)
-        .default("title", "")
-    )
-
-    class_names_shorten = string.shorten(class_names)
+def render_distribution(
+    distribution, class_names, filename, footer=[], header=[], size=8, title=""
+):
+    class_names_short = string.shorten(class_names)
 
     logger.info(
         "graphics.render_distribution({}:{}) -> {}".format(
@@ -43,37 +23,41 @@ def render_distribution(distribution, class_names, filename, options=""):
     )
 
     if len(class_names) != distribution.shape[0]:
-        logger.error("graphics.render_distribution(): shape mismatch.")
+        logger.error("-abcli: graphics: render_distribution(): shape mismatch.")
         return False
 
-    plt.figure(figsize=(options["size"], options["size"]))
+    plt.figure(figsize=(size, size))
 
     plt.bar(range(len(distribution)), distribution, color="#777777")
 
     plt.xticks(
         np.arange(len(class_names)),
-        class_names_shorten,
+        class_names_short,
         rotation="vertical",
     )
 
     if not file.prepare_for_saving(filename):
-        return success
+        return False
 
-    if options["title"]:
-        plt.title(options["title"])
+    if title:
+        plt.title(title)
     plt.grid(True)
 
     plt.savefig(filename)
     plt.close()
 
-    if options["header"] or options["footer"]:
+    if header or footer:
         success, image = file.load_image(filename)
         if not success:
             return success
 
-        image = add_signature(image, options["header"], options["footer"])
+        return file.save_image(
+            filename,
+            add_signature(
+                image,
+                header,
+                footer,
+            ),
+        )
 
-        if not file.save_image(filename, image, options):
-            return False
-
-    return success
+    return True
