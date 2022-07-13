65624f75d7274d4f2a0afa22824612a3258d9e85
kamangir <arash@kamangir.net>
2022-07-13 12:44:35 -0400

    validating fashion_mnist train - kamangir/bolt#689

--- abcli/plugins/graphics/__init__.py
+++ abcli/plugins/graphics/__init__.py
@@ -1,7 +1,8 @@
 from .. import shortname
+
+name = f"{shortname}.plugins.graphics"
+
 from .confusion_matrix import *
 from .distribution import *
 from .signature import *
 from .text import *
-
-name = f"{shortname}.plugins.graphics"
