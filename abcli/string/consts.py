92bc5757492258c45fc4790161f7617b93d94984
kamangir <arash@kamangir.net>
2022-08-18 19:14:30 -0700

    clean-up - kamangir/bolt#697

---
+++ abcli/string/consts.py
@@ -0,0 +1,10 @@
+unit_of = {
+    "ambient light": "{} lux",
+    "humidity": "{} %RH",
+    "iteration": "{:,}",
+    "pitch": "{} deg",
+    "pressure": "{} hPa",
+    "roll": "{} deg",
+    "temperature": "{} 'C",
+    "yaw": "{} deg",
+}
