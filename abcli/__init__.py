cdfb4b2ede8a426f72759251724a21c38bd848cb
kamangir <arash@kamangir.net>
2022-08-31 21:17:56 -0700

    validating black - kamangir/bolt#701

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -2,7 +2,7 @@ import os
 
 NAME = "abcli"
 
-VERSION = "6.1432.1"
+VERSION = "6.1433.1"
 
 
 def fullname():
--- bash/plugins/seed.sh
+++ bash/plugins/seed.sh
@@ -107,7 +107,6 @@ function abcli_seed() {
     seed="${seed}git clone git@github.com:kamangir/awesome-bash-cli.git$delim"
     seed="${seed}cd awesome-bash-cli${delim}"
     seed="${seed}git checkout $abcli_git_branch; git pull$delim"
-    seed="${seed}cd ..$delim_section"
 
     pushd $abcli_path_bash/bootstrap/config > /dev/null
     local filename
