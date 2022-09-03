d268e1f6e0fbe090a1bfbba98b3bb8f668b80fca
kamangir <arash@kamangir.net>
2022-09-02 19:30:45 -0700

    looking for messages from the worker - kamangir/bolt#699

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -2,7 +2,7 @@ import os
 
 NAME = "abcli"
 
-VERSION = "6.1489.1"
+VERSION = "6.1494.1"
 
 
 def fullname():
--- abcli/modules/cookie/__init__.py
+++ abcli/modules/cookie/__init__.py
@@ -1,11 +1,13 @@
 import os
 from abcli import file
 
+NAME = "abcli.modules.cookie"
+
 cookie_filename = os.path.join(os.getenv("abcli_path_cookie", ""), "cookie.json")
 
 _, cookie = file.load_json(cookie_filename, "civilized")
 
-# for item in cookie:
-#    logger.info(f"{NAME}: cookie: {item}: {cookie[item]}")
 
-NAME = "abcli.modules.cookie"
+if cookie.get("cookie.echo", False):
+    for keyword in cookie:
+        print(f"{NAME}: {keyword}: {cookie[keyword]}")
