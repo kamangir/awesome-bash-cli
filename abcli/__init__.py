a953aa3585a16f62b0acaca7a00846ec53031620
kamangir <arash@kamangir.net>
2022-09-08 18:51:43 -0700

    += blue-plugin - kamangir/bolt#700

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -2,7 +2,7 @@ import os
 
 NAME = "abcli"
 
-VERSION = "6.1693.1"
+VERSION = "6.1695.1"
 
 
 def fullname():
--- template/awesome_bash_cli_template/__init__.py
+++
@@ -1,5 +0,0 @@
-NAME = "awesome_bash_cli_template"
-
-VERSION = "1.23.1"
-
-from .functions import *
