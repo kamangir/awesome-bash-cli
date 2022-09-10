67eccced5d718afd64696774f008b49b2eeca0b9
kamangir <arash@kamangir.net>
2022-09-09 18:04:26 -0700

    += blue_plugin - kamangir/bolt#700

--- bash/modules/plugins.sh
+++ bash/modules/plugins.sh
@@ -4,10 +4,10 @@ function abcli_plugins() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ "$task" == "help" ] ; then
-        abcli_help_line "abcli plugins list_of_external" \
-            "show list of expernal plugins."
         abcli_help_line "abcli plugins install [<plugin>]" \
             "install plugin/all plugins."
+        abcli_help_line "abcli plugins list_of_external" \
+            "show list of expernal plugins."
 
         if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
             python3 -m abcli.plugins --help
