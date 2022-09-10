c4f3c995c65a2c9a1850e2fa3f77d86071d32994
kamangir <arash@kamangir.net>
2022-09-10 16:03:55 -0700

    video - kamangir/bolt#700

---
+++ bash/plugins/video.sh
@@ -0,0 +1,63 @@
+#! /usr/bin/env bash
+
+function abcli_create_video() {
+    local task=$(abcli_unpack_keyword $1)
+
+    if [ "$task" == "help" ] ; then
+        abcli_help_line "create_video [<.jpg>] [<filename>] [fps=<10>,rm_frames,scale=<1>]" \
+            "$abcli_object_name/<.jpg> [-> <filename>.mp4]."
+        return
+    fi
+
+    local suffix=$(abcli_clarify_input $1 .jpg)
+
+    local video_filename=${suffix%%.*}
+    local video_filename=$abcli_object_path/$(abcli_clarify_input $2 $video_filename).mp4
+    if [ -f "$video_filename" ] ; then
+        rm $video_filename
+    fi
+
+    local options="$3"
+    local fps=$(abcli_option "$options" fps 10)
+    local rm_frames=$(abcli_option_int "$options" rm_frames 0)
+    local scale=$(abcli_option "$options" scale 1)
+
+    local extension=$(basename -- "$suffix")
+    local extension="${extension##*.}"
+
+    if [ -d "temp_video" ] ; then
+        rm -rf temp_video
+    fi
+    mkdir temp_video
+
+    local filename
+    local index=0
+    local size=""
+    for filename in *$suffix ; do
+        if [ -z "$size" ] ; then
+            local size=$(python3 -c "import abcli.file as file; shape=file.load_image('$filename')[1].shape; print('{}x{}'.format(2*int(shape[1]/2/$scale),2*int(shape[0]/2/$scale)))")
+        fi
+        cp -v $filename $abcli_object_path/temp_video/$index.$extension
+        (( index++ ))
+    done
+
+    abcli_log "$abcli_object_name/$suffix -> $video_filename - $size - $fps fps - 1/$scale"
+
+    ffmpeg \
+        -f image2 \
+        -r $fps \
+        -i $abcli_object_path/temp_video/%d.$extension \
+        -s $size \
+        -vcodec h264 \
+        $video_filename
+
+    cp -v $abcli_object_path/temp_video/0.$extension $abcli_object_path/screenshot.$extension
+
+    if [ "$rm_frames" == "1" ] ; then
+        rm $abcli_object_path/*$suffix
+    fi
+
+    rm -rf $abcli_object_path/temp_video
+
+    abcli_tag set $abcli_object_name video
+}
