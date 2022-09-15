6cd22fd4c6ce88454f335133fb536a10444e7813
kamangir <arash@kamangir.net>
2022-09-14 18:22:48 -0700

    validating unicorn1 - kamangir/bolt#710

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -2,7 +2,7 @@ import os
 
 NAME = "abcli"
 
-VERSION = "6.1873.1"
+VERSION = "7.1874.1"
 
 
 def fullname():
--- bash/modules/abcli.sh
+++ bash/modules/abcli.sh
@@ -17,3 +17,7 @@ function abcli() {
 function abcli_get_version() {
     export abcli_version=$(python3 -c "import abcli; print(abcli.VERSION)")
 }
+
+function fast() {
+    abcli $@
+}
