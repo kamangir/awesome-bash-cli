25ad92d21d41f24f22a58c0a1044ab50da864086
kamangir <arash@kamangir.net>
2022-09-27 19:34:46 -0700

    to stop a looping host - kamangir/bolt#717

--- bash/plugins/session.sh
+++ bash/plugins/session.sh
@@ -96,7 +96,7 @@ function abcli_session() {
             fi
 
             if [ -f "$abcli_path_bash/bootstrap/cookie/disabled" ] ; then
-                printf "abcli is \033[0;31mdisabled\033[0m.\n"
+                abcli_log "abcli is \033[0;31mdisabled\033[0m.\n"
                 return
             fi
 
