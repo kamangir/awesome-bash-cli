2e4351eec85156a069c30ea874d9bc8fec6a26f6
kamangir <arash@kamangir.net>
2022-10-04 18:24:42 -0700

    validating Kanata extract_faces - kamangir/bolt#709

--- abcli/modules/objects.py
+++ abcli/modules/objects.py
@@ -1,5 +1,32 @@
 import os
-from abcli import string
+from abcli import file, string
+from abcli.plugins.storage import instance as storage
+
+
+def list_of_files(object_name, cloud=False, **kwargs):
+    """return list of files in object_name.
+
+    Args:
+        object_name (str): object name.
+        cloud (bool, optional): on cloud. Defaults to False.
+
+    Returns:
+        List[str]: list of files.
+    """
+    return (
+        storage.list_of_objects(
+            object_name,
+            **kwargs,
+        )
+        if cloud
+        else file.list_of(
+            os.path.join(
+                os.getenv("abcli_object_root", ""),
+                object_name,
+            ),
+            **kwargs,
+        )
+    )
 
 
 def signature(info=None):
