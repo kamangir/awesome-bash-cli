33cba56c2524632cfffc5f1850fb23e49ae9ef13
kamangir <arash@kamangir.net>
2022-10-08 10:28:03 -0700

    validating private digest animated - kamangir/bolt#719

---
+++ bash/modules/source.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+function abcli_source_path() {
+    local path=$1
+
+    local options=$2
+    local do_log=$(abcli_option_int "$options" log 0)
+
+    pushd $path > /dev/null
+
+    local filename
+    for filename in *.sh ; do
+        if [ "$do_log" == 1 ] ; then
+            abcli_log "abcli: source: $filename"
+        fi
+
+        source $filename
+    done
+    popd > /dev/null
+}
