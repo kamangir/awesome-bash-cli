7c093a7d6c289ad93936d06af6ce3b1c9feac6f3
kamangir <arash@kamangir.net>
2022-10-15 11:44:48 -0700

    += abcli init - clear - kamangir/bolt#723

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -2,7 +2,7 @@ import os
 
 NAME = "abcli"
 
-VERSION = "7.2093.1"
+VERSION = "7.2094.1"
 
 
 def fullname():
--- bash/modules/init.sh
+++ bash/modules/init.sh
@@ -4,17 +4,17 @@ function abcli_init() {
     local task=$(abcli_unpack_keyword $1)
 
     if [ "$task" == "help" ] ; then
-        abcli_show_usage "abcli init [<plugin>] [~terraform]" \
-            "init [plugin] [and do not terraform]."
+        abcli_show_usage "abcli init [<plugin-name>] [clear,~terraform]" \
+            "init [<plugin-name>]."
         return
     fi
 
     local plugin_name=$(abcli_clarify_input "$1" all)
+    local options=$2
 
     local current_path=$(pwd)
 
     if [ "$plugin_name" == "all" ] ; then
-        local options=$2
         if [ "$abcli_is_mac" == true ] ; then
             local options=$(abcli_option_default "$options" terraform 0)
         fi
@@ -36,5 +36,10 @@ function abcli_init() {
 
     if [[ "$current_path" == "$abcli_path_git"* ]] ; then
         cd $current_path
+    fi
+
+    local do_clear=$(abcli_option_int "$options" clear 0)
+    if [ "$do_clear" == 1 ] ; then
+        clear
     fi
 }
