db72b728f13717c4bc5c788b942bf4a0ff292ea6
kamangir <arash@kamangir.net>
2023-01-04 22:50:37 -0800

    validating the docker image - kamangir/bolt#742

--- bash/plugins/docker.sh
+++ bash/plugins/docker.sh
@@ -4,32 +4,43 @@ function abcli_docker() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ "$task" == "help" ] ; then
-        abcli_show_usage "docker build [run]" \
+        abcli_show_usage "docker build [run,slim]" \
             "build [and run] docker image."
-        abcli_show_usage "docker run" \
+        abcli_show_usage "docker run [slim]" \
             "run docker image."
         return
     fi
 
+    local options=$2
+    local slim=$(abcli_option_int "$options" slim 0)
+
+    local filename="Dockerfile"
+    local tag="kamangir/abcli"
+    if [ "$slim" == 1 ] ; then
+        local filename="Dockerfile-slim"
+        local tag="kamangir/abcli/slim"
+    fi
+
     if [ "$task" == "build" ] ; then
-        local options="$2"
         local do_run=$(abcli_option_int "$options" run 0)
 
         pushd $abcli_path_abcli > /dev/null
 
         mkdir -p temp
         cp -v ~/.kaggle/kaggle.json temp/
-        cp -v ~/.ssh/$abcli_git_ssh_key_name temp/
+
+        abcli_log "docker: building $filename: $tag"
 
         docker build \
-            --build-arg abcli_git_ssh_key_name=$abcli_git_ssh_key_name \
-            -t kamangir/abcli \
+            --build-arg HOME=$HOME \
+            -t $tag \
+            -f $filename \
             .
 
         rm -rf temp
 
         if [ "$do_run" == "1" ] ; then
-            abcli_docker run
+            abcli_docker run $options
         fi
 
         popd > /dev/null
@@ -38,9 +49,11 @@ function abcli_docker() {
     fi
 
     if [ "$task" == "run" ] ; then
+        abcli_log "docker: running $filename: $tag"
+
         pushd $abcli_path_abcli > /dev/null
         # https://gist.github.com/mitchwongho/11266726
-        docker run -it kamangir/abcli /bin/bash
+        docker run -it $tag /bin/bash
         popd > /dev/null
 
         return
