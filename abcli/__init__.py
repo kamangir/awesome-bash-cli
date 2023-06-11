3c6cd8a0c1271f9555896b9a3b24ee99f765f521
kamangir <arash@kamangir.net>
2023-06-10 18:07:03 -0700

    docker refactor - kamangir/bolt#746

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -2,7 +2,7 @@ import os
 
 NAME = "abcli"
 
-VERSION = "7.2267.1"
+VERSION = "7.2268.1"
 
 
 def fullname():
--- bash/modules/initialize.sh
+++ bash/modules/initialize.sh
@@ -5,7 +5,7 @@ function abcli_initialize() {
     git config --global user.name "kamangir"
     git config --global credential.helper store
 
-    abcli_add_ssh_keys
+    [[ "$abcli_is_docker" == false ]] && abcli_add_ssh_keys
 
     export abcli_host_name=$(python3 -m abcli.modules.host get --keyword name)
 
