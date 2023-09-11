d299f7b6fa8aeb8b5fdb07037e0feebbfd97fe8d
kamangir <arash@kamangir.net>
2023-09-10 20:13:34 -0700

    plugin - kamangir/bolt#746

--- bash/modules/select.sh
+++ bash/modules/select.sh
@@ -4,8 +4,8 @@ function abcli_select() {
     local task=$(abcli_unpack_keyword $1)
 
     if [ "$task" == "help" ] ; then
-        abcli_show_usage "abcli select [<object_name>] [open,~trail]" \
-            "select [object_name] [no trail]."
+        abcli_show_usage "abcli select [<object_name>] [open,plugin=<plugin>,~trail]" \
+            "select [object_name]."
         abcli_show_usage "abcli select git_issue <abc>" \
             "select git issue kamangir/bolt#<abc>."
         return
@@ -22,20 +22,25 @@ function abcli_select() {
     local options=$2
     local update_trail=$(abcli_option_int "$options" trail 1)
     local do_open=$(abcli_option_int "$options" open 0)
+    local plugin_name=$(abcli_option "$options" plugin abcli)
 
-    export abcli_object_name_prev=$abcli_object_name
-    export abcli_object_name=$object_name
+    local object_var=${plugin_name}_object_name
+    export ${plugin_name}_object_name_prev=${!object_var}
+    export ${plugin_name}_object_name=$object_name
     
-    export abcli_object_path=$abcli_object_root/$abcli_object_name
-    mkdir -p $abcli_object_path
+    local path=$abcli_object_root/$object_name
+    export ${plugin_name}_object_path=$path
+    mkdir -p $path
 
-    cd $abcli_object_path
+    if [ "$plugin_name" == abcli ] ; then
+        cd $abcli_object_path
 
-    if [ "$update_trail" == 1 ] ; then
-        abcli_trail $abcli_object_path/$abcli_object_name
+        if [ "$update_trail" == 1 ] ; then
+            abcli_trail $abcli_object_path/$abcli_object_name
+        fi
     fi
 
-    abcli_log "📂 $abcli_object_name"
+    abcli_log "📂 $plugin_name: $abcli_object_name"
 
     if [ "$do_open" == 1 ] ; then
         open $abcli_object_path
