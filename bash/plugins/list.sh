c785dd4fef845659aea7f1227dab0a53524ec153
kamangir <arash@kamangir.net>
2023-09-24 13:05:42 -0700

    += list - kamangir/bolt#746

---
+++ bash/plugins/list.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+
+function abcli_list() {
+    local task=$(abcli_unpack_keyword $1 help)
+
+    if [ "$task" == "help" ]; then
+        abcli_show_usage "abcli list cloud|local <object_name>" \
+            "list <object_name>"
+        return
+    fi
+
+    local options=$1
+    local where=$(abcli_option_choice "$options" cloud,local cloud)
+
+    local object_name=$(abcli_clarify_object $2 .)
+
+    if [ "$where" == cloud ]; then
+        local s3_uri=s3://$(abcli_aws_s3_bucket)/$(abcli_aws_s3_prefix)/$object_name/
+        abcli_log "🔗 $s3_uri"
+        aws s3 ls $s3_uri
+    elif [ "$where" == local ]; then
+        local path=$abcli_object_root/$object_name
+        abcli_log "📂 $path"
+        ls $path
+    else
+        abcli_log_error "-abcli: list: $where: location not found."
+        return 1
+    fi
+}
