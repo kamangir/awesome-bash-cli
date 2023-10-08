2af058d85d150d397a6cc603b6a6c12ed69e344d
kamangir <arash@kamangir.net>
2023-10-08 15:43:01 -0700

    objects refactor - kamangir/bolt#746

--- abcli/modules/objects.py
+++ abcli/modules/objects.py
@@ -2,6 +2,14 @@ import os
 from abcli import file, string
 from abcli.plugins.storage import instance as storage
 
+abcli_object_root = os.getenv(
+    "abcli_object_root",
+    os.path.join(
+        os.getenv("HOME"),
+        "storage/abcli",
+    ),
+)
+
 
 def list_of_files(object_name, cloud=False, **kwargs):
     """return list of files in object_name.
@@ -21,7 +29,7 @@ def list_of_files(object_name, cloud=False, **kwargs):
         if cloud
         else file.list_of(
             os.path.join(
-                os.getenv("abcli_object_root", ""),
+                abcli_object_root,
                 object_name,
                 "*",
             ),
@@ -30,23 +38,28 @@ def list_of_files(object_name, cloud=False, **kwargs):
     )
 
 
-def path_of(filename):
+def object_path(object_name="."):
     return os.path.join(
+        abcli_object_root,
         os.getenv(
-            "abcli_object_path",
+            "abcli_object_name",
             "",
-        ),
+        )
+        if object_name == "."
+        else object_name,
+    )
+
+
+def path_of(filename, object_name="."):
+    return os.path.join(
+        object_path(object_name),
         filename,
     )
 
 
 def select(object_name: str):
     os.environ["abcli_object_name"] = object_name
-    os.environ["abcli_object_path"] = os.path.join(
-        os.getenv("HOME"),
-        "storage/abcli",
-        object_name,
-    )
+    os.environ["abcli_object_path"] = object_path(object_name)
 
 
 def signature(info=None):
