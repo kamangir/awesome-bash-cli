2182853b240aa8fa237bbe6ac282950ceca7312b
kamangir <arash@kamangir.net>
2023-10-15 12:15:52 -0700

    @pytest refactor - kamangir/bolt#746

--- bash/plugins/pytest.sh
+++ bash/plugins/pytest.sh
@@ -6,7 +6,7 @@ function abcli_pytest() {
     local plugin_name=$(abcli_option "$options" plugin abcli)
 
     if [ $(abcli_option_int "$options" help 0) == 1 ]; then
-        local options="dryrun,list,~log,plugin=<plugin-name>"
+        local options="dryrun,list,~log,plugin=<plugin-name>,warning"
         abcli_show_usage "$plugin_name pytest$ABCUL[$options]$ABCUL[args]" \
             "pytest $plugin_name."
         return
@@ -16,6 +16,9 @@ function abcli_pytest() {
 
     [[ $(abcli_option_int "$options" list 0) == 1 ]] &&
         local args="--collect-only"
+
+    [[ $(abcli_option_int "$options" warning 0) == 0 ]] &&
+        local args="$args --disable-warnings"
 
     # https://stackoverflow.com/a/40720333/17619982
     abcli_eval "path=$abcli_path_git/$plugin_name,$options" \
