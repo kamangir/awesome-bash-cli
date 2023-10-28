aeab4fbc46aa8d889f81516282dd4f649d412640
kamangir <arash@kamangir.net>
2023-10-27 18:32:02 -0700

    @seed sagemaker - kamangir/bolt#420

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -2,7 +2,7 @@ import os
 
 NAME = "abcli"
 
-VERSION = "7.2396.1"
+VERSION = "7.2397.1"
 
 
 def fullname():
---
+++ assets/aws/sagemaker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Start an interactive session
+bash -i
+
+# Run your desired commands here
+echo "Hello, world!"
+ls -l
+date
+
+# Keep the session open
