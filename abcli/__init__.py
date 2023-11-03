7a39e73d2f4aed7169c23211814aa539cb08334a
kamangir <arash@kamangir.net>
2023-11-02 20:53:03 -0700

    syntax error fix - kamangir/bolt#420

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -2,7 +2,7 @@ import os
 
 NAME = "abcli"
 
-VERSION = "7.2435.1"
+VERSION = "7.2436.1"
 
 
 def fullname():
--- bash/modules/plugins.sh
+++ bash/modules/plugins.sh
@@ -3,7 +3,7 @@
 function abcli_plugins() {
     local task=$(abcli_unpack_keyword $1 help)
 
-    if [ "$task" == "help" ] ; then
+    if [ "$task" == "help" ]; then
         abcli_show_usage "abcli plugins install [<plugin-name>]" \
             "install <plugin-name>|all plugins."
         abcli_show_usage "abcli plugins is_present <plugin-name>" \
@@ -11,31 +11,31 @@ function abcli_plugins() {
         abcli_show_usage "abcli plugins list_of_external" \
             "show list of expernal plugins."
 
-        if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
+        if [ "$(abcli_keyword_is $2 verbose)" == true ]; then
             python3 -m abcli.plugins --help
         fi
         return
     fi
 
-    if [ $task == "install" ] ; then
+    if [ $task == "install" ]; then
         local plugin_name=$(abcli_unpack_keyword "$2")
 
-        if [ -z "$plugin_name" ] ; then
-            local $plugin_name
-            for plugin_name in $(abcli_plugins list_of_external --log 0 --delim space) ; do
+        if [ -z "$plugin_name" ]; then
+            local plugin_name
+            for plugin_name in $(abcli_plugins list_of_external --log 0 --delim space); do
                 abcli_plugins install $plugin_name
             done
         else
             local repo_name=$(echo "$plugin_name" | tr _ -)
-            pushd $abcli_path_git/$repo_name > /dev/null
+            pushd $abcli_path_git/$repo_name >/dev/null
             pip3 install -e .
-            popd > /dev/null
+            popd >/dev/null
         fi
 
         return
     fi
 
-    if [ $task == "is_present" ] ; then
+    if [ $task == "is_present" ]; then
         local plugin_name=$2
 
         local list_of_plugins=$(abcli_plugins list_of_external --delim , --log 0)
@@ -44,11 +44,10 @@ function abcli_plugins() {
         return
     fi
 
-
-    if [ $task == "list_of_external" ] ; then
+    if [ $task == "list_of_external" ]; then
         python3 -m abcli.plugins \
-                    list_of_external \
-                    ${@:2}
+            list_of_external \
+            ${@:2}
         return
     fi
 
