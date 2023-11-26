ef688d6d418caf29dd7b0a180d6b84f3ecd06f87
kamangir <arash@kamangir.net>
2023-11-26 13:21:21 -0800

    formatter - kamangir/bolt#123

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -2,7 +2,7 @@ import os
 
 NAME = "abcli"
 
-VERSION = "7.2502.1"
+VERSION = "7.2503.1"
 
 DESCRIPTION = "🚀 a language to speak AI."
 
--- bash/plugins/GPU.sh
+++ bash/plugins/GPU.sh
@@ -3,17 +3,17 @@
 function abcli_gpu() {
     local task=$(abcli_unpack_keyword $1 help)
 
-    if [ "$task" == "help" ] ; then
+    if [ "$task" == "help" ]; then
         abcli_show_usage "abcli gpu validate" \
             "validate gpu."
 
-        if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
+        if [ "$(abcli_keyword_is $2 verbose)" == true ]; then
             python3 -m abcli.plugins.gpu --help
         fi
         return
     fi
 
-    if [ $task == "validate" ] ; then
+    if [ $task == "validate" ]; then
         nvidia-smi
         python3 -m abcli.plugins.gpu \
             validate \
