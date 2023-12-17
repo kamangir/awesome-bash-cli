cb0e9eeb54c676880aa9f7c37283e6f0cbf8abf4
kamangir <arash@kamangir.net>
2023-12-16 19:00:16 -0800

    roofAI inference invoke refactor - kamangir/bolt#123

--- bash/plugins/browse.sh
+++ bash/plugins/browse.sh
@@ -1,13 +1,22 @@
 #! /usr/bin/env bash
 
 function abcli_browse_url() {
-    if [ "$abcli_is_mac" == true ] ; then
-        open "$1"
-    elif [ "$abcli_is_ec2" == true ] ; then
-        firefox "$1"
-    elif [ "$abcli_is_rpi" == true ] ; then
-        DISPLAY=:0 chromium-browser -kiosk --no-sandbox "$1"
+    local url=$1
+
+    if [[ "$url" == "help" ]]; then
+        abcli_show_usage "abcli browse_url$ABCUL<url>" \
+            "browse <url>."
+        return
+    fi
+
+    abcli_log "🔗 $url"
+    if [ "$abcli_is_mac" == true ]; then
+        open "$url"
+    elif [ "$abcli_is_ec2" == true ]; then
+        firefox "$url"
+    elif [ "$abcli_is_rpi" == true ]; then
+        DISPLAY=:0 chromium-browser -kiosk --no-sandbox "$url"
     else
-        abcli_log_error "-abcli: browse_url: $1: failed."
-    fi 
+        abcli_log_error "-abcli: browse_url: $url: failed."
+    fi
 }
