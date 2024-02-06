6bbbc3a0eb2b231dd8d5e410db9c72e3364c9b4a
kamangir <arash@kamangir.net>
2024-02-05 19:50:12 -0800

    semseg on SageMaker - kamangir/bolt#123

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -2,7 +2,7 @@ import os
 
 NAME = "abcli"
 
-VERSION = "7.2671.1"
+VERSION = "7.2675.1"
 
 DESCRIPTION = "🚀 a language to speak AI."
 
---
+++ abcli/elapsed_timer.py
@@ -0,0 +1,23 @@
+from abcli import string
+import time
+
+
+class ElapsedTimer(object):
+    def __init__(self):
+        self.start_time = time.time()
+        self.elapsed_time = None
+
+    def stop(self):
+        if self.start_time is None:
+            return
+
+        self.elapsed_time = time.time() - self.start_time
+        self.start_time = None
+
+    def elapsed_pretty(self, **kwargs):
+        self.stop()
+        return (
+            "None"
+            if self.elapsed_time is None
+            else string.pretty_duration(self.elapsed_time, **kwargs)
+        )
