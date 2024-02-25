f826eeb4354846b788b1dd6377ca0fc85ca40e2a
kamangir <arash@kamangir.net>
2024-02-24 23:31:21 -0800

    pylint refactor - kamangir/bolt#1234

---
+++ bash/plugins/pip.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+function abcli_pip() {
+    local task=$1
+
+    if [[ "$task" == "help" ]]; then
+        abcli_show_usage "abcli pip install <module>" \
+            "install <module>."
+        return
+    fi
+
+    if [ "$task" == "install" ]; then
+        local module=$2
+
+        python3 -c "import $module" &>/dev/null ||
+            pip3 install $module
+
+        return
+    fi
+
+    abcli_log_error "-abcli: pip: $task: command not found."
+}
--- bash/plugins/pylint.sh
+++ bash/plugins/pylint.sh
@@ -1,17 +1,17 @@
 #! /usr/bin/env bash
 
-export abcli_pylint_ignored=W1203,C0103,C0111,C0114,C0115,C0116,C0411,W0404,W0237,C0209,C0415,W0621,W0702,W0102,W1202,E0401,W1514,C3002,W0401,W0611,C0413,C0412,W0603,R0911,E1101,W0622,R1721,W0718,R1728,C3001,R0801,R0401,R0914,R0913,R0915,W0123,R0912,C0301,W0511,W0105,W0613,R0902,R0903,R1735,W1401,W3101
+export abcli_pylint_ignored=W1203,C0103,C0111,C0114,C0115,C0116,C0411,W0404,W0237,C0209,C0415,W0621,W0702,W0102,W1202,E0401,W1514,C3002,W0401,W0611,C0413,C0412,W0603,R0911,E1101,W0622,R1721,W0718,R1728,C3001,R0801,R0401,R0914,R0913,R0915,W0123,R0912,C0301,W0511,W0105,W0613,R0902,R0903,R1735,W1401,W3101,W1308,E1102
 
 function abcli_pylint() {
     local options=$1
 
     if [ $(abcli_option_int "$options" help 0) == 1 ]; then
-        abcli_show_usage "abcli pylint$ABCUL[<args>]" \
+        abcli_show_usage "abcli pylint [<args>]" \
             "pylint abcli."
         return
     fi
 
-    pip3 install pylint
+    abcli_pip install pylint
 
     pushd $abcli_path_git/awesome-bash-cli >/dev/null
     pylint \
