240d208ff20a935054c39d957664dcb62a5ded2e
kamangir <arash@kamangir.net>
2024-03-23 17:38:35 -0700

    @pause refactor - kamangir/bolt#746

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -2,7 +2,7 @@ import os
 
 NAME = "abcli"
 
-VERSION = "7.2935.1"
+VERSION = "7.2939.1"
 
 DESCRIPTION = "🚀 a language to speak AI."
 
--- bash/plugins/pause.sh
+++ bash/plugins/pause.sh
@@ -1,13 +1,19 @@
 #! /usr/bin/env bash
 
 function abcli_pause() {
-    local message=${1:-press any key to continue...}
+    local options=$1
 
-    if [ "$message" == help ]; then
-        abcli_show_usage "abcli pause <message>" \
+    if [ $(abcli_option_int "$options" help 0) == 1 ]; then
+        abcli_show_usage "abcli pause [dryrun|<message>]" \
             "show <message> and pause for key press."
         return
     fi
+
+    if [[ $(abcli_option_int "$options" dryrun 0) == 1 ]]; then
+        return
+    fi
+
+    local message=${1:-press any key to continue...}
 
     abcli_log "$message"
     read -p ""
