917b199231cea5ec28dd5c5b440ee72ac9b859bb
kamangir <arash@kamangir.net>
2024-03-25 19:32:51 -0700

    @T disable fix - kamangir/bolt#746

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -2,7 +2,7 @@ import os
 
 NAME = "abcli"
 
-VERSION = "7.2941.1"
+VERSION = "7.2942.1"
 
 DESCRIPTION = "🚀 a language to speak AI."
 
--- bash/abcli.sh
+++ bash/abcli.sh
@@ -47,7 +47,7 @@ function abcli_main() {
     fi
 }
 
-if [ -f "$abcli_path_ignore/disabled" ]; then
+if [ -f "$abcli_path_bash/../assets/ignore/disabled" ]; then
     printf "abcli is \033[0;31mdisabled\033[0m.\n"
 else
     abcli_main "$@"
