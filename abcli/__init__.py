ee2ef8b942a15eb82c4eec0d5477a393d59974a0
kamangir <arash@kamangir.net>
2024-04-02 18:46:22 -0700

    pipeline fix - kamangir/bolt#746

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -2,7 +2,7 @@ import os
 
 NAME = "abcli"
 
-VERSION = "7.2950.1"
+VERSION = "7.2951.1"
 
 DESCRIPTION = "🚀 a language to speak AI."
 
--- abcli/tests/test_env.py
+++ abcli/tests/test_env.py
@@ -4,4 +4,4 @@ from abcli import env
 def test_abcli_env():
     assert env.abcli_path_home
     assert env.abcli_path_git
-    assert env.storage
+    assert env.abcli_path_storage
