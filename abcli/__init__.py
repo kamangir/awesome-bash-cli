d9f013a2867854b4ef42847360d35ac86a2de2b5
kamangir <arash@kamangir.net>
2024-04-20 16:58:30 -0700

    @git++ - kamangir/bolt#746

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -2,7 +2,7 @@ import os
 
 NAME = "abcli"
 
-VERSION = "8.39.1"
+VERSION = "8.40.1"
 
 DESCRIPTION = "🚀 a language to speak AI."
 
@@ -13,8 +13,4 @@ def fullname() -> str:
         VERSION,
         f"-{abcli_git_branch}" if abcli_git_branch else "",
     )
-
-
-
-
 
