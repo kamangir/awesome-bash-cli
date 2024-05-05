d437baa0ea00b7c58d13db5ea0352dc0ff5341a3
kamangir <arash@kamangir.net>
2024-05-05 16:14:36 -0700

    @sleep refactor - kamangir/bolt#746

--- bash/plugins/sleep.sh
+++ bash/plugins/sleep.sh
@@ -11,6 +11,6 @@ function abcli_sleep() {
 
     local seconds=$(abcli_option "$options" seconds 3)
 
-    abcli_log_local "pausing for $seconds s ... (^C to stop)"
+    abcli_log_local "sleeping for $seconds s ... (^C to stop)"
     sleep $seconds
 }
