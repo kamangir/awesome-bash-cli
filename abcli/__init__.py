43ff1eca132bdb3c7920d4bef47ccbd83d7e736b
kamangir <arash@kamangir.net>
2024-05-05 15:39:59 -0700

    @watch refactor - kamangir/bolt#746

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -2,7 +2,7 @@ import os
 
 NAME = "abcli"
 
-VERSION = "8.131.1"
+VERSION = "8.132.1"
 
 DESCRIPTION = "🚀 a language to speak AI."
 
--- bash/plugins/watch.sh
+++ bash/plugins/watch.sh
@@ -4,17 +4,21 @@ function abcli_watch() {
     local options=$1
 
     if [ $(abcli_option_int "$options" help 0) == 1 ]; then
-        options="dryrun,seconds=<seconds>"
+        options="~clear,dryrun,seconds=<seconds>"
         abcli_show_usage "@watch [$options]$ABCUL<command-line>" \
             "watch <command-line>"
         return
     fi
+
+    local do_clear=$(abcli_option_int "$options" clear 1)
 
     local command_line="${@:2}"
 
     abcli_log "watching $command_line ..."
 
     while true; do
+        [[ "$do_clear" == 1 ]] && clear
+
         abcli_eval ,$options "$command_line"
 
         abcli_sleep ,$options
