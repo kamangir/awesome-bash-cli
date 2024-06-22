7aa1a1d6d62bd1a4638e68fa24505f6d183e3715
kamangir <arash@kamangir.net>
2024-06-21 18:19:44 -0700

    checking in - kamangir/bolt#746

--- abcli/.abcli/plugins/pypi/browse.sh
+++ abcli/.abcli/plugins/pypi/browse.sh
@@ -7,16 +7,20 @@ function abcli_pypi_browse() {
 
     if [ $(abcli_option_int "$options" help 0) == 1 ]; then
         local callable=$([[ "$plugin_name" == "abcli" ]] && echo "@" || echo "$plugin_name ")
-        options=""
-        [[ "$plugin_name" == abcli ]] && options="${EOP}plugin=<plugin-name>$EOPE"
-        abcli_show_usage "${callable}pypi browse $options" \
+        options="token"
+        [[ "$plugin_name" == abcli ]] && options="$options$EOP,plugin=<plugin-name>$EOPE"
+        abcli_show_usage "${callable}pypi browse$ABCUL$options" \
             "browse pypi/$plugin_name."
         return
     fi
 
+    local do_token=$(abcli_option_int "$options" token 0)
+
     local module_name=$(abcli_get_module_name_from_plugin $plugin_name)
 
-    abcli_browse "https://pypi.org/project/$module_name/"
+    local url="https://pypi.org/project/$module_name/"
+    [[ "$do_token" == 1 ]] &&
+        url="https://pypi.org/manage/account/token/"
 
-    return 0
+    abcli_browse $url
 }
