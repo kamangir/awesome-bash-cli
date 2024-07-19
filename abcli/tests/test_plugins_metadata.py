ff09f84449b0b34d6651a844dd70f74a3b2ee494
kamangir <arash@kamangir.net>
2024-07-18 18:00:04 -0700

    fascinating feature 🪄 - kamangir/bolt#746

--- abcli/tests/test_plugins_metadata.py
+++ abcli/tests/test_plugins_metadata.py
@@ -1,6 +1,17 @@
 import pytest
 from typing import Callable
-from abcli.plugins.metadata.functions import get, post, MetadataSourceType
+from abcli.plugins.metadata import (
+    get,
+    post,
+    get_from_file,
+    get_from_object,
+    get_from_path,
+    post,
+    post_to_file,
+    post_to_object,
+    post_to_path,
+    MetadataSourceType,
+)
 from abcli.modules.objects import unique_object, object_path, path_of
 from abcli.string import random_
 
@@ -121,3 +132,40 @@ def test_metadata_dict():
             dict_values=True,
         )
     ) == sorted(value.values())
+
+
+def test_metadata_file():
+    object_name = unique_object()
+    key = random_()
+    value = random_()
+
+    filename = path_of(
+        filename="metadata.yaml",
+        object_name=object_name,
+    )
+
+    assert post_to_file(filename, key, value)
+
+    assert get_from_file(filename, key) == value
+
+
+def test_metadata_object():
+    object_name = unique_object()
+    key = random_()
+    value = random_()
+
+    assert post_to_object(object_name, key, value)
+
+    assert get_from_object(object_name, key) == value
+
+
+def test_metadata_path():
+    object_name = unique_object()
+    key = random_()
+    value = random_()
+
+    path = object_path(object_name=object_name, create=True)
+
+    assert post_to_path(path, key, value)
+
+    assert get_from_path(path, key) == value
