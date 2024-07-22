717ab3777f9534ef4d01f34eda0acc0ac8108552
kamangir <arash@kamangir.net>
2024-07-21 21:45:36 -0700

    pipeline fix, hopefully - kamangir/bolt#746

--- abcli/tests/test_plugins_markdown.py
+++ abcli/tests/test_plugins_markdown.py
@@ -2,7 +2,7 @@ from abcli.string import random_
 from abcli.plugins.markdown import generate_table
 
 
-def test_generate_table(repo_names: bool):
+def test_generate_table():
     assert generate_table(
         [random_() for _ in range(50)],
         cols=4,
