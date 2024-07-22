0de3beb66092989cd3a35a3568fd1290749a77e1
kamangir <arash@kamangir.net>
2024-07-21 21:02:25 -0700

    pipeline fix, hopefuly - kamangir/bolt#746

--- abcli/tests/test_plugins_markdown.py
+++ abcli/tests/test_plugins_markdown.py
@@ -3,4 +3,7 @@ from abcli.plugins.markdown import generate_table
 
 
 def test_generate_table(repo_names: bool):
-    assert generate_table([random_() for _ in range(50)], col=4)
+    assert generate_table(
+        [random_() for _ in range(50)],
+        cols=4,
+    )
