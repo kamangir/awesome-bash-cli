417359a8c14ad65d36ad48e4f17959b0949cb1aa
kamangir <arash@kamangir.net>
2024-08-23 18:59:13 -0700

    doc fix - kamangir/bolt#746

--- abcli/logger.py
+++ abcli/logger.py
@@ -5,7 +5,7 @@ from logging.handlers import RotatingFileHandler
 # to hide "botocore.credentials Found credentials in environment variables."
 logging.getLogger("botocore.credentials").setLevel(logging.WARNING)
 
-ABCUL = "\\\n\t"
+ABCUL = " \\\n\t"
 
 abcli_log_filename = env.abcli_log_filename
 
