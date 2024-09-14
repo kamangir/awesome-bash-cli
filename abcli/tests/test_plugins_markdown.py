af07e793d08ba95fea2a6a2f9ea66a8c546b31a4
kamangir <arash@kamangir.net>
2024-09-13 17:45:54 -0700

    -> blue-objects - kamangir/bolt#746

--- abcli/tests/test_plugins_markdown.py
+++
@@ -1,9 +0,0 @@
-from blue_options.string import random
-from abcli.plugins.markdown import generate_table
-
-
-def test_generate_table():
-    assert generate_table(
-        [random() for _ in range(50)],
-        cols=4,
-    )
