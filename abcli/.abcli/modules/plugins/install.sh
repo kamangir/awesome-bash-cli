30a1af221da79e1518f095a756061754ca4c0cd6
kamangir <arash@kamangir.net>
2025-03-12 19:49:21 -0700

    start of SageMaker-refactors-2025-03-12-sthpwx 🪄 - kamangir/bolt#746

--- abcli/.abcli/modules/plugins/install.sh
+++ abcli/.abcli/modules/plugins/install.sh
@@ -3,6 +3,24 @@
 function abcli_plugins_install() {
     local plugin_name=$(abcli_unpack_keyword "$1")
 
+    if [[ -z "$plugin_name" ]]; then
+        pushd $abcli_path_git >/dev/null
+        for dir in */; do
+            if ! find "$dir" -type d -name ".abcli" -print -quit | read; then
+                continue
+            fi
+
+            abcli_log "$dir ..."
+
+            cd $dir
+            pip3 install -e .
+            pip3 install -r requirements.txt
+            cd ..
+        done
+        popd >/dev/null
+        return
+    fi
+
     local repo_name=$(abcli_get_repo_name_from_plugin $plugin_name)
     if [[ -z "$repo_name" ]]; then
         abcli_log_error "@plugins: install: $plugin_name: plugin not found."
